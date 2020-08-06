02a8e6245d716b3711f2f283fc1e6353dce9b8de
janprill <jan.prill@gmail.com>
2020-08-06 21:33:54 +0200

    DSGVO backend (wip)

--- src/main/java/com/remoteanwalt/application/ExtractionService.java
+++ src/main/java/com/remoteanwalt/application/ExtractionService.java
@@ -1,13 +1,47 @@
 package com.remoteanwalt.application;
 
 import com.remoteanwalt.domain.jaxb.ACTType;
+import com.remoteanwalt.domain.jaxb.DATEType;
+import com.remoteanwalt.domain.jaxb.NOTEType;
+import com.remoteanwalt.domain.jaxb.QUOTSTARTType;
+
+import javax.xml.bind.JAXBElement;
 
 public class ExtractionService {
     public ExtractionService(final ACTType act) {
         act.getPREAMBLE().getGRCONSID().getCONSID().stream().forEach(consid -> {
+            System.out.println(consid.getNP().getNOP());
             consid.getNP().getTXT().getContent().stream().forEach(content -> {
-                System.out.println(content);
                 System.out.println("============================");
+                if (content.getClass().equals(String.class)) {
+                    System.out.println(content);
+                } else {
+                    JAXBElement element = (JAXBElement) content;
+                    if (element.getDeclaredType().equals(NOTEType.class)) {
+                        NOTEType note = (NOTEType) element.getValue();
+                        System.out.println("NOTE: " + note.getTYPE() + " " + note.getP().getContent());
+                    } else if (element.getDeclaredType().equals(DATEType.class)) {
+                        DATEType date = (DATEType) element.getValue();
+                        System.out.println(date.getISO());
+                    } else if (element.getDeclaredType().equals(QUOTSTARTType.class)) {
+                        QUOTSTARTType start = (QUOTSTARTType) element.getValue();
+                        System.out.println(start.getCODE());
+                    } else {
+                        System.out.println(content);
+                    }
+                }
+
+            });
+        });
+
+        act.getENACTINGTERMS().getDIVISION().stream().forEach(division -> {
+            System.out.println(division.getTITLE().getTI());
+            System.out.println(division.getTITLE().getSTI().getP().getContent());
+            System.out.println("==========================================");
+            division.getARTICLE().stream().forEach(article -> {
+                System.out.println(article.getIDENTIFIER());
+                System.out.println(article.getTIART());
+                System.out.println(article.getSTIART().getContent());
             });
         });
     }
--- src/main/resources/data/dsgvo.xslt
+++ src/main/resources/data/dsgvo.xslt
@@ -2,12 +2,21 @@
 <xsl:stylesheet version="3.0"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
+
+  <xsl:output method="xml" indent="yes"/>
+  <xsl:strip-space elements="*"/>
+
+
   <xsl:template match="ACT">
-    <xsl:apply-templates />
+    <root>
+      <xsl:apply-templates />
+    </root>
   </xsl:template>
 
   <xsl:template match="PREAMBLE">
-    <xsl:apply-templates />
+    <preamble>
+      <xsl:apply-templates />
+    </preamble>
   </xsl:template>
 
   <xsl:template match="GR.CONSID">
@@ -26,12 +35,42 @@
     <number>
       <xsl:value-of select="text()" />
     </number>
+    <xsl:apply-templates />
+  </xsl:template>
+
+  <xsl:template match="TXT">
+    <TXT>
+      <xsl:copy>
+        <xsl:apply-templates select="@* | node()"/>
+      </xsl:copy>
+    </TXT>
+  </xsl:template>
+
+  <xsl:template match="NOTE">
+    <note>
+      <xsl:apply-templates />
+    </note>
+  </xsl:template>
+
+  <xsl:template match="P">
+    <p>
+      <xsl:apply-templates />
+    </p>
+  </xsl:template>
+
+  <xsl:template match="text()">
+    <text>
+      <xsl:value-of select="."/>
+    </text>
   </xsl:template>
 
   <xsl:template match="@* | node()">
-    <!-- <xsl:copy>
+    <xsl:value-of select="./text()" />
+    <!--
+    <xsl:copy>
       <xsl:apply-templates select="@* | node()"/>
-    </xsl:copy> -->
+    </xsl:copy>
+    -->
   </xsl:template>
 
 </xsl:stylesheet>
